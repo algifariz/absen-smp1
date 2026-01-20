7c706b1c3f79636bfe231c54ab825c931265abda
Ikram-Maulana <hey@ikram.is-a.dev>
2026-01-21 06:38:46 +0700

    refactor: improve flex responsiveness and clean up next.config
    - use fluid flex values in siswa header components
    - remove unused allowedDevOrigins development config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.1.3:3000",
-    "http://192.168.100.127:3000",
-    "http://192.168.100.127",
-  ],
 };
 
 module.exports = nextConfig;
